71290780e07aa00412cff296bbdd6aabcacfbc3f
ZhangWei2222 <985983437@qq.com>
2017-07-17 09:14:08 +0800

    fist homepage

---
+++ personal homepage/ME.js
@@ -0,0 +1,44 @@
+function panda()
+{
+	document.all.background.style.display="block";
+	document.all.background.style.width=document.body.clientWidth;
+	document.all.background.style.height=document.body.clientHeight;
+	document.all.layer1.style.display="block";
+}
+
+function  close11()
+{
+	document.all.background.style.display='none';
+	document.all.layer1.style.display='none';
+	return false;
+}
+
+function dolphin()
+{
+	document.all.background.style.display="block";
+	document.all.background.style.width=document.body.clientWidth;
+	document.all.background.style.height=document.body.clientHeight;
+	document.all.layer2.style.display="block";
+}
+
+function  close22()
+{
+	document.all.background.style.display='none';
+	document.all.layer2.style.display='none';
+	return false;
+}
+
+function elephant()
+{
+	document.all.background.style.display="block";
+	document.all.background.style.width=document.body.clientWidth;
+	document.all.background.style.height=document.body.clientHeight;
+	document.all.layer3.style.display="block";
+}
+
+function  close33()
+{
+	document.all.background.style.display='none';
+	document.all.layer3.style.display='none';
+	return false;
+}
